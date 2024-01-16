0603df625094783989946705441421b0ce345034
Encapsulateed <alexey.mitroshkin@gmail.com>
2024-01-16 22:49:55 +0300

    хуй пизда жопа член запупа пенис

--- lab11/lab_11_Митрошкин.sql
+++ lab11/lab_11_Митрошкин.sql
@@ -22,7 +22,6 @@ ON
 	FILEGROWTH = 5
 );
 GO
-USE lab_11_db;
 
 /*
 1. Создать базу данных, спроектированную в рамках лабораторной работы №4, используя изученные в лабораторных работах
@@ -39,6 +38,13 @@ USE lab_11_db;
 - назначение ограничений целостности (PRIMARY KEY, NULL/NOT NULL/UNIQUE, CHECK и т.п.);
 - определение значений по умолчанию;
 */
+USE lab_11_db;
+
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[Event]') AND type in (N'U'))
+DROP TABLE [dbo].[Event]
+
 
 CREATE TABLE Event
 (
@@ -47,22 +53,36 @@ CREATE TABLE Event
     Description NVARCHAR(1000) NOT NULL,
     PhotoAlbumLink NVARCHAR(2048) NOT NULL,
     DocumentsLink NVARCHAR(2048) NOT NULL,
-    StartDate DATETIME NOT NULL CHECK (StartDate <= EndDate AND StartDate BETWEEN '2023-01-01' AND '2999-12-31'),
-    EndDate DATETIME NOT NULL CHECK (StartDate <= EndDate AND EndDate BETWEEN '2023-01-01' AND '2999-12-31'),
+    StartDate DATETIME NOT NULL,
+    EndDate DATETIME NOT NULL,
     MediaMentionLink NVARCHAR(2048) DEFAULT 'cmr.bmstu.ru',
-    Venue NVARCHAR(2048) NOT NULL
+    Venue NVARCHAR(2048) NOT NULL,
+    CONSTRAINT CHK_StartEndDates CHECK (StartDate <= EndDate AND StartDate BETWEEN '2023-01-01' AND '2999-12-31'),
+    CONSTRAINT CHK_ValidStartDate CHECK (StartDate BETWEEN '2023-01-01' AND '2999-12-31'),
+    CONSTRAINT CHK_ValidEndDate CHECK (EndDate BETWEEN '2023-01-01' AND '2999-12-31')
 );
+
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[Hardatons]') AND type in (N'U'))
+DROP TABLE [dbo].[Hardatons]
 
 CREATE TABLE Hardatons
 (
     EventId INTEGER PRIMARY KEY NOT NULL,
-    StartDateApplications DATETIME NOT NULL CHECK (StartDateApplications <= ResultAnnouncementDate AND StartDateApplications BETWEEN '2023-01-01' AND '2999-12-31'),
-    ResultAnnouncementDate DATETIME NOT NULL CHECK (StartDateApplications <= ResultAnnouncementDate AND ResultAnnouncementDate BETWEEN '2023-01-01' AND '2999-12-31'),
+    StartDateApplications DATETIME NOT NULL,
+    ResultAnnouncementDate DATETIME NOT NULL,
     OrganizerPhotoLink NVARCHAR(2048) NULL,
     OrganizerMessage NVARCHAR(MAX) NULL,
     ContestTaskLink NVARCHAR(300) NOT NULL,
-    FOREIGN KEY (EventId) REFERENCES Event(EventId)
+    CONSTRAINT CHK_ValidDates CHECK (StartDateApplications <= ResultAnnouncementDate AND StartDateApplications BETWEEN '2023-01-01' AND '2999-12-31')
 );
+
+
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[ClassicEvent]') AND type in (N'U'))
+DROP TABLE [dbo].[ClassicEvent]
 
 CREATE TABLE ClassicEvent
 (
@@ -71,6 +91,10 @@ CREATE TABLE ClassicEvent
     FOREIGN KEY (EventId) REFERENCES Event(EventId)
 );
 
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[Partner]') AND type in (N'U'))
+DROP TABLE [dbo].[Partner]
 
 CREATE TABLE Partner
 (
@@ -80,6 +104,10 @@ CREATE TABLE Partner
     WebsiteLink NVARCHAR(2048) NOT NULL
 );
 
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[EventPartner]') AND type in (N'U'))
+DROP TABLE [dbo].[EventPartner]
 
 CREATE TABLE EventPartner
 (
@@ -90,6 +118,24 @@ CREATE TABLE EventPartner
     FOREIGN KEY (PartnerID) REFERENCES Partner(PartnerID)
 );
 
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[Team]') AND type in (N'U'))
+DROP TABLE [dbo].[Team]
+
+CREATE TABLE Team
+(
+    TeamId INTEGER PRIMARY KEY NOT NULL,
+    Captain INTEGER,
+    Name NVARCHAR(20) UNIQUE NOT NULL,
+    Motto NVARCHAR(100) NULL,
+  
+);
+
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[Participant]') AND type in (N'U'))
+DROP TABLE [dbo].[Participant]
 
 CREATE TABLE Participant
 (
@@ -99,19 +145,21 @@ CREATE TABLE Participant
     FullName NVARCHAR(200) NOT NULL,
     GroupName NVARCHAR(10) NOT NULL,
     Specialization NVARCHAR(30) NULL,
-    FOREIGN KEY (TeamId) REFERENCES Team(TeamId) ON DELETE SET NULL
 );
 
 
-CREATE TABLE Team
-(
-    TeamId INTEGER PRIMARY KEY NOT NULL,
-    Captain INTEGER,
-    Name NVARCHAR(20) UNIQUE NOT NULL,
-    Motto NVARCHAR(100) NULL,
-    FOREIGN KEY (Captain) REFERENCES Participant(ParticipantID) ON DELETE SET NULL
-);
+ALTER TABLE Team
+ADD CONSTRAINT FK_Team_Captain
+FOREIGN KEY (Captain) REFERENCES Participant(ParticipantId) ON DELETE SET NULL;
 
+ALTER TABLE Participant
+ADD CONSTRAINT FK_Participant_Team
+FOREIGN KEY (TeamId) REFERENCES Team(TeamId);
+
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[TeamEvent]') AND type in (N'U'))
+DROP TABLE [dbo].[TeamEvent]
 
 CREATE TABLE TeamEvent
 (
@@ -122,6 +170,10 @@ CREATE TABLE TeamEvent
     FOREIGN KEY (EventId) REFERENCES Event(EventId)
 );
 
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[Project]') AND type in (N'U'))
+DROP TABLE [dbo].[Project]
 
 CREATE TABLE Project
 (
