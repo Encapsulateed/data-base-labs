c779d1a193ea17b139821ecf9928cddfed687374
Encapsulateed <alexey.mitroshkin@gmail.com>
2024-01-17 14:51:14 +0300

    💀

--- lab11/lab_11_Митрошкин.sql
+++ lab11/lab_11_Митрошкин.sql
@@ -45,6 +45,50 @@ FROM sys.objects
 WHERE object_id = OBJECT_ID(N'[dbo].[Event]') AND type in (N'U'))
 DROP TABLE [dbo].[Event]
 
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[Hardatons]') AND type in (N'U'))
+DROP TABLE [dbo].[Hardatons]
+
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[ClassicEvent]') AND type in (N'U'))
+DROP TABLE [dbo].[ClassicEvent]
+
+
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[Partner]') AND type in (N'U'))
+DROP TABLE [dbo].[Partner]
+
+
+
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[EventPartner]') AND type in (N'U'))
+DROP TABLE [dbo].[EventPartner]
+
+
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[Project]') AND type in (N'U'))
+DROP TABLE [dbo].[Project]
+
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[Team]') AND type in (N'U'))
+DROP TABLE [dbo].[Team]
+
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[Participant]') AND type in (N'U'))
+DROP TABLE [dbo].[Participant]
+
+IF  EXISTS (SELECT *
+FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[TeamEvent]') AND type in (N'U'))
+DROP TABLE [dbo].[TeamEvent]
+
 
 CREATE TABLE Event
 (
@@ -53,19 +97,23 @@ CREATE TABLE Event
     Description NVARCHAR(1000) NOT NULL,
     PhotoAlbumLink NVARCHAR(2048) NOT NULL,
     DocumentsLink NVARCHAR(2048) NOT NULL,
-    StartDate DATETIME NOT NULL,
-    EndDate DATETIME NOT NULL,
+    StartDate DATE NOT NULL,
+    EndDate DATE NOT NULL,
     MediaMentionLink NVARCHAR(2048) DEFAULT 'cmr.bmstu.ru',
     Venue NVARCHAR(2048) NOT NULL,
-    CONSTRAINT CHK_StartEndDates CHECK (StartDate <= EndDate AND StartDate BETWEEN '2023-01-01' AND '2999-12-31'),
+    CONSTRAINT CHK_StartEndDates CHECK (StartDate <= EndDate),
     CONSTRAINT CHK_ValidStartDate CHECK (StartDate BETWEEN '2023-01-01' AND '2999-12-31'),
     CONSTRAINT CHK_ValidEndDate CHECK (EndDate BETWEEN '2023-01-01' AND '2999-12-31')
 );
 
-IF  EXISTS (SELECT *
-FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[Hardatons]') AND type in (N'U'))
-DROP TABLE [dbo].[Hardatons]
+CREATE TABLE ClassicEvent
+(
+    EventId INTEGER PRIMARY KEY NOT NULL,
+    RegistrationLink NVARCHAR(2048) NOT NULL,
+    FOREIGN KEY (EventId) REFERENCES Event(EventId) ON DELETE CASCADE
+);
+
+
 
 CREATE TABLE Hardatons
 (
@@ -75,26 +123,239 @@ CREATE TABLE Hardatons
     OrganizerPhotoLink NVARCHAR(2048) NULL,
     OrganizerMessage NVARCHAR(MAX) NULL,
     ContestTaskLink NVARCHAR(300) NOT NULL,
-    CONSTRAINT CHK_ValidDates CHECK (StartDateApplications <= ResultAnnouncementDate AND StartDateApplications BETWEEN '2023-01-01' AND '2999-12-31')
+    CONSTRAINT CHK_ValidDates CHECK (StartDateApplications <= ResultAnnouncementDate AND StartDateApplications BETWEEN '2023-01-01' AND '2999-12-31'),
+    FOREIGN KEY (EventId) REFERENCES Event(EventId) ON DELETE CASCADE
+
 );
 
 
-IF  EXISTS (SELECT *
-FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[ClassicEvent]') AND type in (N'U'))
-DROP TABLE [dbo].[ClassicEvent]
+GO
+DROP VIEW IF EXISTS EventClassicEventView
+GO
 
-CREATE TABLE ClassicEvent
-(
-    EventId INTEGER PRIMARY KEY NOT NULL,
-    RegistrationLink NVARCHAR(2048) NOT NULL,
-    FOREIGN KEY (EventId) REFERENCES Event(EventId)
-);
+CREATE VIEW EventClassicEventView
+AS
+    SELECT e.EventID,
+        e.Name,
+        e.Description,
+        e.StartDate,
+        e.EndDate,
+        e.MediaMentionLink,
+        e.PhotoAlbumLink,
+        e.Venue,
+        ce.RegistrationLink
+    FROM Event AS e JOIN ClassicEvent AS ce ON e.EventId = ce.EventId
+GO
 
-IF  EXISTS (SELECT *
-FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[Partner]') AND type in (N'U'))
-DROP TABLE [dbo].[Partner]
+GO
+CREATE TRIGGER trg_insert_classicEvent ON  EventClassicEventView INSTEAD OF INSERT
+AS
+    BEGIN
+    IF EXISTS(SELECT 1
+    FROM Event
+    WHERE EventId IN (SELECT EventId
+    FROM Event))
+    BEGIN
+        RAISERROR('Такой EventId уже существует, вставка невозможна!',10,1)
+        ROLLBACK;
+    END;
+
+    INSERT INTO Event
+        (EventId,Name,Description, StartDate, EndDate, MediaMentionLink,PhotoAlbumLink,Venue)
+    SELECT EventId, Name, Description, StartDate, EndDate, MediaMentionLink, PhotoAlbumLink, Venue
+    FROM inserted;
+
+    INSERT INTO ClassicEvent
+        (EventId,RegistrationLink)
+    SELECT EventId, RegistrationLink
+    FROM inserted;
+END 
+
+GO
+CREATE TRIGGER trg_delete_classicEvent ON EventClassicEventView INSTEAD OF DELETE
+AS
+BEGIN
+    IF NOT EXISTS(SELECT 1
+    FROM Event
+    WHERE EventId IN (SELECT EventId
+    FROM Event))
+    BEGIN
+        RAISERROR('Такой EventId не существует, удаление невозможно ',10,2)
+        ROLLBACK;
+    END;
+
+
+    DELETE FROM Event WHERE EventId IN (SELECT EventId
+    FROM deleted)
+END
+GO
+
+CREATE TRIGGER trg_update_classicEvent ON EventClassicEventView INSTEAD OF UPDATE AS
+BEGIN
+    IF NOT EXISTS(SELECT 1
+    FROM Event
+    WHERE EventId IN (SELECT EventId
+    FROM inserted))
+    BEGIN
+        RAISERROR('Такой EventId не существует, обновление невозможно ',10,2)
+        ROLLBACK;
+    END;
+
+    IF(UPDATE(EventId))
+    BEGIN
+        RAISERROR('Поле EventId недоступно для изменения!',10,3)
+        ROLLBACK;
+    END;
+     IF UPDATE(Name) OR
+       UPDATE(Description) OR
+       UPDATE(PhotoAlbumLink) OR
+       UPDATE(DocumentsLink) OR
+       UPDATE(StartDate) OR
+       UPDATE(EndDate) OR
+       UPDATE(MediaMentionLink) OR
+       UPDATE(Venue)
+    BEGIN
+        RAISERROR('Event fields are immutable throughout the view', 16, 1);
+        RETURN;
+    END;
+
+    -- Обновление данных в ClassicEvent
+    WITH UpdatedClassicEvent AS (
+        SELECT
+            CE.EventId,
+            CE.RegistrationLink
+        FROM INSERTED I
+        INNER JOIN ClassicEvent CE ON I.EventId = CE.EventId
+    )
+    UPDATE CE
+    SET
+        RegistrationLink = U.RegistrationLink
+    FROM ClassicEvent CE
+    INNER JOIN UpdatedClassicEvent U ON CE.EventId = U.EventId;
+
+END
+
+GO
+DROP VIEW IF EXISTS EventHardatonView
+GO
+
+CREATE VIEW EventHardatonView
+AS
+    SELECT e.EventID,
+        e.Name,
+        e.Description,
+        e.StartDate, e.EndDate,
+        e.MediaMentionLink, e.PhotoAlbumLink,
+        e.Venue,
+        h.ContestTaskLink,
+        h.OrganizerMessage,
+        h.OrganizerPhotoLink,
+        h.StartDateApplications,
+        h.ResultAnnouncementDate
+    FROM Event AS e JOIN Hardatons AS h ON e.EventId = h.EventId
+GO
+
+GO
+CREATE TRIGGER trg_insert_hardaton ON  EventHardatonView INSTEAD OF INSERT
+AS
+    BEGIN
+    IF EXISTS(SELECT 1
+    FROM Event
+    WHERE EventId IN (SELECT EventId
+    FROM Event))
+    BEGIN
+        RAISERROR('Такой EventId уже существует, вставка невозможна!',10,1)
+        ROLLBACK;
+    END;
+
+    INSERT INTO Event
+        (EventId,Name,Description, StartDate, EndDate, MediaMentionLink,PhotoAlbumLink,Venue)
+    SELECT EventId, Name, Description, StartDate, EndDate, MediaMentionLink, PhotoAlbumLink, Venue
+    FROM inserted;
+
+    INSERT INTO Hardatons
+        (EventId,ContestTaskLink,OrganizerMessage,OrganizerPhotoLink,StartDateApplications,ResultAnnouncementDate)
+    SELECT ContestTaskLink, OrganizerMessage, OrganizerPhotoLink, StartDateApplications, ResultAnnouncementDate
+    FROM inserted;
+END 
+
+GO
+CREATE TRIGGER trg_delete_hardaton ON EventHardatonView INSTEAD OF DELETE
+AS
+BEGIN
+    IF NOT EXISTS(SELECT 1
+    FROM Event
+    WHERE EventId IN (SELECT EventId
+    FROM Event))
+    BEGIN
+        RAISERROR('Такой EventId не существует, удаление невозможно ',10,2)
+        ROLLBACK;
+    END;
+
+
+    DELETE FROM Event WHERE EventId IN (SELECT EventId
+    FROM deleted)
+END
+GO
+
+CREATE TRIGGER trg_update_hardaton ON EventHardatonView INSTEAD OF UPDATE AS
+BEGIN
+    IF NOT EXISTS(SELECT 1
+    FROM Event
+    WHERE EventId IN (SELECT EventId
+    FROM inserted))
+    BEGIN
+        RAISERROR('Такой EventId не существует, обновление невозможно ',10,2)
+        ROLLBACK;
+    END;
+
+    IF(UPDATE(EventId))
+    BEGIN
+        RAISERROR('Поле EventId недоступно для изменения!',10,3)
+        ROLLBACK;
+    END;
+
+    IF UPDATE(Name) OR
+        UPDATE(Description) OR
+        UPDATE(PhotoAlbumLink) OR
+        UPDATE(DocumentsLink) OR
+        UPDATE(StartDate) OR
+        UPDATE(EndDate) OR
+        UPDATE(MediaMentionLink) OR
+        UPDATE(Venue)
+    BEGIN
+        RAISERROR('Поля мероприятий неизменяемы во всем представлении.', 16, 1);
+        RETURN;
+    END;
+
+    -- Обновление данных в Hardatons
+    WITH
+        UpdatedHardatons
+        AS
+        (
+            SELECT
+                H.EventId,
+                H.StartDateApplications,
+                H.ResultAnnouncementDate,
+                H.OrganizerPhotoLink,
+                H.OrganizerMessage,
+                H.ContestTaskLink
+            FROM INSERTED I
+                INNER JOIN Hardatons H ON I.EventId = H.EventId
+        )
+    UPDATE H
+    SET
+        StartDateApplications = U.StartDateApplications,
+        ResultAnnouncementDate = U.ResultAnnouncementDate,
+        OrganizerPhotoLink = U.OrganizerPhotoLink,
+        OrganizerMessage = U.OrganizerMessage,
+        ContestTaskLink = U.ContestTaskLink
+    FROM Hardatons H
+        INNER JOIN UpdatedHardatons U ON H.EventId = U.EventId;
+
+END
+
+GO
 
 CREATE TABLE Partner
 (
@@ -103,11 +364,6 @@ CREATE TABLE Partner
     Photo NVARCHAR(2048) NOT NULL,
     WebsiteLink NVARCHAR(2048) NOT NULL
 );
-
-IF  EXISTS (SELECT *
-FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[EventPartner]') AND type in (N'U'))
-DROP TABLE [dbo].[EventPartner]
 
 CREATE TABLE EventPartner
 (
@@ -118,10 +374,6 @@ CREATE TABLE EventPartner
     FOREIGN KEY (PartnerID) REFERENCES Partner(PartnerID)
 );
 
-IF  EXISTS (SELECT *
-FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[Team]') AND type in (N'U'))
-DROP TABLE [dbo].[Team]
 
 CREATE TABLE Team
 (
@@ -129,13 +381,9 @@ CREATE TABLE Team
     Captain INTEGER,
     Name NVARCHAR(20) UNIQUE NOT NULL,
     Motto NVARCHAR(100) NULL,
-  
+
 );
 
-IF  EXISTS (SELECT *
-FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[Participant]') AND type in (N'U'))
-DROP TABLE [dbo].[Participant]
 
 CREATE TABLE Participant
 (
@@ -148,19 +396,6 @@ CREATE TABLE Participant
 );
 
 
-ALTER TABLE Team
-ADD CONSTRAINT FK_Team_Captain
-FOREIGN KEY (Captain) REFERENCES Participant(ParticipantId) ON DELETE SET NULL;
-
-ALTER TABLE Participant
-ADD CONSTRAINT FK_Participant_Team
-FOREIGN KEY (TeamId) REFERENCES Team(TeamId);
-
-IF  EXISTS (SELECT *
-FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[TeamEvent]') AND type in (N'U'))
-DROP TABLE [dbo].[TeamEvent]
-
 CREATE TABLE TeamEvent
 (
     TeamId INTEGER,
@@ -169,11 +404,6 @@ CREATE TABLE TeamEvent
     FOREIGN KEY (TeamId) REFERENCES Team(TeamId),
     FOREIGN KEY (EventId) REFERENCES Event(EventId)
 );
-
-IF  EXISTS (SELECT *
-FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[Project]') AND type in (N'U'))
-DROP TABLE [dbo].[Project]
 
 CREATE TABLE Project
 (
@@ -188,3 +418,30 @@ CREATE TABLE Project
 );
 
 
+ALTER TABLE Team
+ADD CONSTRAINT FK_Team_Captain
+FOREIGN KEY (Captain) REFERENCES Participant(ParticipantId) ON DELETE SET NULL;
+
+ALTER TABLE Participant
+ADD CONSTRAINT FK_Participant_Team
+FOREIGN KEY (TeamId) REFERENCES Team(TeamId);
+
+
+INSERT INTO Event
+    (EventId,Name,Description,PhotoAlbumLink,DocumentsLink,StartDate,EndDate,Venue)
+VALUES(
+        1, 'ГЕЙ ТУСА', 'gaY PARTYY YEAH', 'WWW.GAY.RU', 'GAY.DOC', '2023-01-12', '2023-01-12', 'гей бар')
+
+
+INSERT INTO ClassicEvent
+    (EventId,RegistrationLink)
+VALUES
+    (1, 'геи.reg')
+
+DELETE FROM ClassicEvent WHERE EventId = 1;
+
+SELECT *
+FROM Event;
+
+SELECT *
+from ClassicEvent;
